b4a5876882fbe88b6752ba58801a100642b3f2d4
Alejandro Rincón <jalejandrorincon19@hotmail.com>
2021-04-17 12:04:33 -0500

    chat finalizado v1

--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -19,6 +19,7 @@ io.on('connection', (client) => {
         client.join(data.sala);
 
         usuarios.agregarPersona(client.id, data.nombre, data.sala);
+        client.broadcast.to(data.sala).emit('crearMensaje', crearMensaje('Administrador', `${ data.nombre } se unio`));
 
         client.broadcast.to(data.sala).emit('listaPersona', usuarios.getPersonasPorSala(data.sala));
 
@@ -26,13 +27,15 @@ io.on('connection', (client) => {
         //console.log({ personas });
     });
 
-    client.on('crearMensaje', (data) => {
+    client.on('crearMensaje', (data, callback) => {
 
         let persona = usuarios.getPersona(client.id);
 
         let mensaje = crearMensaje(persona.nombre, data.mensaje);
         client.broadcast.to(persona.sala).emit('crearMensaje', mensaje);
 
+
+        callback(mensaje);
     });
 
     client.on('disconnect', () => {
